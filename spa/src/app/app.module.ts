f6abf6073998e52eb713ac86ae5c009ce04774f1
ElCattivo13 <elcattivo@posteo.de>
2021-07-16 03:13:01 +0200

    added TranslateModule to app moddule

--- spa/src/app/app.module.ts
+++ spa/src/app/app.module.ts
@@ -16,6 +16,13 @@ import { TableCardComponent } from './components/table-card/table-card.component
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -32,6 +39,15 @@ import { MatIconModule } from '@angular/material/icon';
     AppRoutingModule,
     FontAwesomeModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'de'
+    }),
     MatSidenavModule,
     MatCardModule,
     MatToolbarModule,
